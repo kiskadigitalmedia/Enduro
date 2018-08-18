c14d9ec0c93f8131e29b009a8d3f03efc4b69447
adriandudau <adrian@ro-deals.uk>
2018-08-18 02:09:54 +0100

    webpack bundler javascript

--- libs/actions/start.js
+++ libs/actions/start.js
@@ -7,6 +7,7 @@ const action = function () {}
 const enduro_server = require(enduro.enduro_path + '/libs/enduro_server/enduro_server')
 
 action.prototype.action = function () {
+	enduro.config.webpackmode_prod = true;
 	return enduro_server.run()
 }
 
